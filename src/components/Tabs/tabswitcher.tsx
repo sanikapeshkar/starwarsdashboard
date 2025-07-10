9427994385d71af78cd596dbd7337296bf4021fa
Sanika Peshkar <sanikapeshkar480@gmail.com>
2025-07-10 20:40:30 +0530

    feat : created a tab switcher component | added shadcn tab

---
+++ src/components/Tabs/tabswitcher.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { ReactNode, useState } from "react";
+import { TabSwitcherProps } from "./tabs.types";
+
+
+export default function TabSwitcher({ tabs, onTabChange }: TabSwitcherProps) {
+  const [activeTab, setActiveTab] = useState(tabs[0]?.value || "");
+
+  const handleChange = (value: string) => {
+    setActiveTab(value);
+    onTabChange?.(value);
+  };
+
+  return (
+    <Tabs value={activeTab} onValueChange={handleChange} className="w-full">
+      <TabsList className="mb-4">
+        {tabs.map((tab) => (
+          <TabsTrigger key={tab.value} value={tab.value}>
+            {tab.label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+
+      {tabs.map(
+        (tab) =>
+          tab.content && (
+            <TabsContent key={tab.value} value={tab.value}>
+              {tab.content}
+            </TabsContent>
+          )
+      )}
+    </Tabs>
+  );
+}
